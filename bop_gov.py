7b069a70ce07d125a4f896cb904bd423029e4f31
mmarifat <mma.rifat66@gmail.com>
2020-03-09 05:23:11 +0600

    #time exception

--- bop_gov.py
+++ bop_gov.py
@@ -15,29 +15,20 @@ import time
 import re
 from os import path
 
-def main():
-    # check for existing files
-    if not path.exists("out.csv"):
-        with open("out.csv", 'w', newline='', encoding='utf-8') as csv_file:
-            fieldnames = ['No', 'Name', 'Release', 'Address1', 'Address2', 'Address3', 'Address4']
-            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
 
-    # take input search no's
-    with open('in.csv', mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        no = []
-        for row in csv_reader:
-            number = re.sub("[^0-9]", "", (f'\t{row["Name"]}'))
-            no.append(number)
-    scrap_data(no)
+# check for existing files
+if not path.exists("out.csv"):
+    with open("out.csv", 'w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = ['No', 'Name', 'Release', 'Address1', 'Address2', 'Address3', 'Address4']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
 
 def scrap_data(numbers):
     # initialize
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.bop.gov/inmateloc/")
 
-    wait = WebDriverWait(driver, 20)
+    wait = WebDriverWait(driver, 10)
     for count, no in enumerate(numbers, 1):
         print("************************\nNow doing: ", no)
 
@@ -51,29 +42,23 @@ def scrap_data(numbers):
 
         # if not available in the csv , add it via browsing
         if not no in check:
+            search = wait.until(EC.element_to_be_clickable((By.ID, 'inmNumber')))
+            search.clear()
+            search.send_keys(no)
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button#searchNumber'))).click()
             try:
-                search = wait.until(EC.element_to_be_clickable((By.ID, 'inmNumber')))
-                search.clear()
-                search.send_keys(no)
-                wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button#searchNumber'))).click()
-                try:
-                    name = wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDName'))).text
-                    rd = wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDBot2'))).text
-                    if rd != "LIFE":
-                        release_date = re.sub("[^0-9,/]", "", rd)
-                    else:
-                        release_date = "LIFE"
-
-                    con2 = [no, name, release_date]
-                    wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDBot1'))).click()
-                except:
-                    continue
+                name = wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDName'))).text
+                rd = wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDBot2'))).text
+                release_date = re.sub("[^0-9,/]", "", rd)
+                con2 = [no, name, release_date]
+                wait.until(EC.element_to_be_clickable((By.ID, 'numResultTDBot1'))).click()
 
                 # find if further info available or not
                 try:
                     driver.switch_to.window(driver.window_handles[1])
                     wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Inmate Mail"))).click()
-                    address = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='address-item']/div"))).text
+                    address = wait.until(
+                        EC.element_to_be_clickable((By.XPATH, "//div[@class='address-item']/div"))).text
 
                     # manipulation
                     con = re.sub("[\n]", ",", address).rsplit(",")
@@ -87,27 +72,29 @@ def scrap_data(numbers):
                     driver.switch_to.window(driver.window_handles[0])
                 except:
                     con = [""]
+            except:
+                continue
 
-                # append all info from single search
-                content = [con2 + con]
-                print(content)
-                print("Completed : ", count, "\n************************\n")
+            # append all info from single search
+            content = [con2 + con]
+            print(content)
+            print("Completed : ", count, "\n************************\n")
 
-                # write into a csv with unicode support
-                with open("out.csv", 'a', newline='', encoding='utf-8') as csvfile:
-                    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL, delimiter=',')
-                    writer.writerows(content)
-            except TimeoutException as time:
-                driver.quite()
-                main()
-
+            # write into a csv with unicode support
+            with open("out.csv", 'a', newline='', encoding='utf-8') as csvfile:
+                writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL, delimiter=',')
+                writer.writerows(content)
         else:
             print("Data already available")
 
     # close and finish
-    driver.close()
+    driver.quite()
 
-if __name__ == "__main__":
-    main()
-
-
+# take input search no's
+with open('in.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    no = []
+    for row in csv_reader:
+        number = re.sub("[^0-9]", "", (f'\t{row["Name"]}'))
+        no.append(number)
+scrap_data(no)
